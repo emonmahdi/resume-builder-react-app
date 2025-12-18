b1772854835d7371d24dd584a16478a735a96240
emonmahdi <developeremon1997@gmail.com>
2025-12-18 10:55:59 +0600

    tailwind setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
- 
-import './App.css'
-
-function App() { 
-
+import "./App.css";
+function App() {
   return (
     <>
-      <h3>Resume Builder React App</h3>
+      <h3 className="bg-orange-500 p-4 text-2xl font-bold text-center">
+        Resume Builder React App
+      </h3>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
